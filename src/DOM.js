667c9f195911ddc1aebcbe4dff9e67f3605bdff3
GrigoriyKostryukov <kostryukov-grigoriy@mail.ru>
2022-10-24 22:56:10 +0300

    Resolve task

--- src/DOM.js
+++ src/DOM.js
@@ -1,3 +1,5 @@
+import { createElement } from 'react';
+
 /*
   В функцию appendToBody передаются 3 параметра:
   tag - имя тега, content - содержимое тега и count - количество вставок.
@@ -5,6 +7,10 @@
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
 export function appendToBody(tag, content, count) {
+    let elemTemplate = `<${tag}>${content}</${tag}>`;
+    for (let i = 0; i < count; ++i) {
+        document.body.insertAdjacentHTML('beforeend', elemTemplate);
+    }
 }
 
 /*
@@ -15,9 +21,24 @@ export function appendToBody(tag, content, count) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function generateTree(childrenCount, level) {
+    return generateChildren(1, level, childrenCount);
 }
 
-/*
+function generateChildren(levelNumber, totalLevels, childrenCount) {
+    let element = document.createElement('div');
+    element.className = `item_${levelNumber}`;
+    if (levelNumber < totalLevels) {
+        for (let j = 0; j < childrenCount; ++j) {
+            element.append(
+                generateChildren(levelNumber + 1, totalLevels, childrenCount),
+            );
+        }
+    }
+
+    return element;
+}
+
+/* 
   Используйте функцию для создания дерева тегов DIV из предыдущего задания.
   Создайте дерево с вложенностью 3 и числом элементов в каждом узле 2.
   Далее замените все узлы второго уровня (т.е. имеющие класс item_2) на теги SECTION.
@@ -26,4 +47,13 @@ export function generateTree(childrenCount, level) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function replaceNodes() {
+    let tree = generateTree(2, 3);
+    let itemsToReplace = tree.getElementsByClassName('item_2');
+    for (let item of itemsToReplace) {
+        let newItem = document.createElement('section');
+        newItem.innerHTML = item.innerHTML;
+        newItem.className = item.className;
+        item.replaceWith(newItem);
+    }
+    return tree;
 }
